d1e1733fc2d8aded60632b85b93434b9b60ce661
LittleMoon215 <littlemoon215@mail.ru>
2021-05-03 20:09:39 +0700

    fix

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SpacePuzzleLegends
+{
+    public class UIController : VisualElement
+    {
+        private VisualElement mainMenuScreen;
+        private VisualElement settingsScreen;
+
+        private Button settingsButton, howToPlayButton, playButton;
+
+        private void OnEnable()
+        {
+            var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
+
+            settingsButton = rootVisualElement.Q<Button>("settings-button");
+            playButton = rootVisualElement.Q<Button>("play-button");
+            howToPlayButton = rootVisualElement.Q<Button>("how-to-play-button");
+
+            settingsButton.RegisterCallback<ClickEvent>(ev => GoSettingsTask());
+        }
+
+        private void GoSettingsTask()
+        {
+
+        }
+
+        private void GoHowToPlayTask()
+        {
+
+        }
+
+        private void GoPlayTask()
+        {
+
+        }
+    }
+}
