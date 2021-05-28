57f65b31043fc3010e04cbc4e731fd726836618d
AndrevvRoman <andrey_romanof@mail.ru>
2021-05-28 19:02:59 +0700

    лью кашу

--- Library/PackageCache/com.unity.textcore@1.0.0-preview.2/Scripts/Editor/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-// Allow internal visibility to the following assemblies
-
-#if UNITY_EDITOR
-[assembly: InternalsVisibleTo("Unity.TextMeshPro.Editor")]
-#endif
